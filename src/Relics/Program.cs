b4f765716b2f1bcd1145b4f1171696ccb78d8337
Kevin Norris <kjnorris1205@hotmail.com>
2025-11-19 22:19:34 -0500

    Create movie model and scaffold controller, views

--- src/Relics/Program.cs
+++ src/Relics/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Relics.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<RelicsDbContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("RelicsDbContext") ?? throw new InvalidOperationException("Connection string 'RelicsDbContext' not found.")));
+}
+else
+{
+    builder.Services.AddDbContext<RelicsDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionRelicsDbContext") ?? throw new InvalidOperationException("Connection string 'ProductionRelicsDbContext' not found.")));
+}
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
